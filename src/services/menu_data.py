5bc651436b27befe55f7e794940f84008481a6bf
Daniel Saraiva <danieldsvn@gmail.com>
2023-04-14 14:31:14 -0300

    Implementado classe MenuData

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,43 @@
-# Req 3
+from models.dish import Dish
+from models.ingredient import Ingredient
+import csv
+
+
+def get_dishes_from_csv(path):
+    with open(path, encoding="utf8") as file:
+        data = csv.reader(file, delimiter=",", quotechar='"')
+
+        dishes = {}
+        for index, item in enumerate(data):
+            if index > 0:
+                if item[0] in dishes:
+                    dishes[item[0]]["ingredients"].append(
+                        (item[2], int(item[3]))
+                    )
+                else:
+                    dishes[item[0]] = {}
+                    dishes[item[0]]["price"] = float(item[1])
+                    dishes[item[0]]["ingredients"] = []
+                    dishes[item[0]]["ingredients"].append(
+                        (item[2], int(item[3]))
+                    )
+
+        return dishes
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = self.get_dishes(source_path)
+
+    def get_dishes(self, path):
+        dishes_dictionary = get_dishes_from_csv(path)
+        dishes_set = set()
+
+        for dish_name in dishes_dictionary:
+            dish = Dish(dish_name, dishes_dictionary[dish_name]["price"])
+            for ingredient, qty in dishes_dictionary[dish_name]["ingredients"]:
+                ingredient_instance = Ingredient(ingredient)
+                dish.add_ingredient_dependency(ingredient_instance, qty)
+            dishes_set.add(dish)
+
+        return dishes_set
